92178b7274b68a8b090837303ca27cc6ced6d477
神楽坂紫 <nashimotomasayuki@gmail.com>
2016-02-14 01:30:01 +0800

    尝试上传……失败

--- uploadimgtosmmstest/uploadimgtosmmstest/ViewController.swift
+++ uploadimgtosmmstest/uploadimgtosmmstest/ViewController.swift
@@ -10,37 +10,107 @@ import UIKit
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
+    @IBOutlet weak var 按钮: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func 按按钮(sender: UIButton) {
+        加载相册图片()
+    }
     
-    func showpic()
+    func 加载相册图片()
     {
-        imagePicker.delegate=self
-        imagePicker.sourceType=UIImagePickerControllerSourceType.PhotoLibrary
-        imagePicker.modalTransitionStyle=UIModalTransitionStyle.CoverVertical
-        imagePicker.allowsEditing=true
-        self.presentViewController(imagePicker, animated:true, completion: nil)
+        let 相册图片 = UIImagePickerController()
+        相册图片.delegate = self
+        相册图片.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        相册图片.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+        相册图片.allowsEditing = true
+        self.presentViewController(相册图片, animated:true, completion: nil)
         
     }
-    UIImagePickerControllerDelegate 代理
-    func imagePickerController(picker:UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject :AnyObject])
-    {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         self.dismissViewControllerAnimated(true, completion:nil);
-        let gotImage=info[UIImagePickerControllerOriginalImage]as UIImage
-        upload(gotImage)//上传
+        let gotImage = image
+         upload(gotImage)//上传
+
         
     }
 
-    
     //上传
+    
+//    func 上传图片(img:UIImage){
+//        
+//        let 请求地址 = NSURL(string: "https://sm.ms/api/upload")
+//        let 上传请求 = NSURLRequest(URL: 请求地址!)
+//        
+//        let 请求 = NSURLSession.sharedSession()
+//        
+//        let 上传内容 = UIImagePNGRepresentation(img)
+//        
+
+//        }
+//        
+//        上传任务.resume()
+//    }
+    
     func upload(img:UIImage)
     {
 
-        let uploadurl:String="https://sm.ms/api/upload"//设置服务器接收地址
-        let request=NSMutableURLRequest(URL:NSURL(string:uploadurl)!)
-        request.HTTPMethod="POST"//设置请求方式
+        let 上传地址:String="https://sm.ms/api/upload"
+        let 上传请求 = NSMutableURLRequest(URL:NSURL(string:上传地址)!)
+        上传请求.HTTPMethod="POST"
+
+        let boundary:String = "-nyaacat"
+        let contentType:String = "multipart/form-data;boundary="+boundary
+        上传请求.addValue(contentType, forHTTPHeaderField: "Content-Type")
+        
+        let 上传内容 = UIImagePNGRepresentation(img)
+        
+        let 请求 = NSURLSession.sharedSession()  //这个地方不太会喵……
+
+        let 上传任务 = 请求.uploadTaskWithRequest(上传请求, fromData: 上传内容){
+            (data:NSData?, reponse:NSURLResponse?, error:NSError?) ->Void in
+            print("done")
+
+            if(error != nil){
+                print(error)
+            } else{
+                let 回应:String = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
+                print(回应)
+            }
+            
+        }
+        
+        上传任务.resume()
+        
+        
+//        
+//        上传数据.appendData(NSString(format:"\r\n--\(boundary)\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+//        
+//        上传数据.appendData(NSString(format:"Content-Disposition:form-data;name=\"userfile\";filename=\"dd.jpg\"\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+//        
+//        上传数据.appendData(NSString(format:"Content-Type:application/octet-stream\r\n\r\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+//        
+//        上传数据.appendData(图片数据!)
+//        
+//        上传数据.appendData(NSString(format:"\r\n--\(boundary)").dataUsingEncoding(NSUTF8StringEncoding)!)
+//        
+//        上传请求.HTTPBody = 上传数据
+//        
+//        let 队列 = NSOperationQueue()
+//        
+//        NSURLConnection.sendAsynchronousRequest(上传请求, queue: 队列) { (回复, 数据, error) -> Void in
+//            if(error != nil){
+//                print(error)
+//            } else{
+//                let 回应:String = NSString(data: 数据!, encoding: NSUTF8StringEncoding) as! String
+//                print(回应)
+//            }
+//            
+//        }
         
     }
     
